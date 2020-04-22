8b9ed81b5e1a0be0588241c54b407cad8be55a90
infernezor@gmail.com <infernezor@gmail.com>
2020-04-22 11:27:28 -0400

    basic structure for datamodeling. Elephant SQL changed to match.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
+
+const DataBase = require('../models/functions');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/api/joinbar', (req, res) => {
   res.send('respond with a resource');
+  //grabs sessionID from database.
+  //generates token.
+  //sends token and session id.
+});
+
+router.post('/api/createbar', async (req, res) => {
+  res.send('Responded.');
+  const { sessionID, password, name } = req.body;
+  const response = await DataBase.addSession(sessionID, password, name);
+  console.log(response);
+  return response;
 });
 
 module.exports = router;
