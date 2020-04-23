d6b076ab0736cad126ea7216fc38cbe484e41f1c
Zach Barbre <zach.barbre@gmail.com>
2020-04-23 10:46:04 -0400

    joinbar creates tokens. need to get the return to the client working correctly

--- routes/api.js
+++ routes/api.js
@@ -10,30 +10,20 @@ const API_SECRET = process.env.OT_API_SECRET; // these should grab from the env 
 const opentok = new OpenTok(API_KEY, API_SECRET);
 
 /* GET users listing. */
-router.get('/joinbar', (req, res) => {
-  res.send('respond with a resource');
-
-  let sessionId;
-  let token;
-  opentok.createSession({}, (error, session) => {
-    if (error) {
-      console.log('Error creating session:', error);
-    } else {
-      sessionId = session.sessionId;
-      console.log('Session ID: ', sessionId);
-      //  Use the role value appropriate for the user:
-      const tokenOptions = {};
-      tokenOptions.role = 'publisher';
-      tokenOptions.data = 'username=bob';
-
-      // Generate a token.
-      token = opentok.generateToken(sessionId, tokenOptions);
-      console.log(token);
-    }
-  });
+router.post('/joinbar', async (req, res) => {
+  const { joinBar, password } = req.body;
+  // let token;
+  const response = await DataBase.getByBarName(joinBar);
+  console.log(response);
+  const sessionId = response.sessionid;
+  // Generate a token. Token options possible for later
+  // const tokenOptions = {};
+  // tokenOptions.role = 'publisher';
+  // tokenOptions.data = 'username=bob';
+  const token = opentok.generateToken(sessionId);
 
   // sends token and session id.
-  return token;
+  res.json({ sessionId, token }).status(200);
 });
 
 router.post('/createbar', (req, res) => {
@@ -42,7 +32,7 @@ router.post('/createbar', (req, res) => {
 
   opentok.createSession((err, session) => {
     if (err) {
-      console.log(err);
+      console.log('Error creating session:', err);
     } else {
       newSession = session.sessionId;
       const response = DataBase.addSession(barName, newSession, password);
